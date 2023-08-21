eb48d2da2d6a65e169a0896d6b0815243c13dd9b
Abanoub Samy <abanoubsamy2341@gmail.com>
2023-08-21 01:48:00 -0700

    trial

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,5 @@
-#include <unistd.h>
 #include "main.h"
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -8,7 +8,21 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+
+int _putchar(int c)
 {
-	return (write(1, &c, 1));
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+
+	if (c != BUF_FLUSH)
+	{
+		buf[i++] = c;
+	}
+	return (1);
 }
